9a600b6775f047385e87ccf5a068d299a6688bff
mfocqueteau <mfocqueteau@uc.cl>
2022-04-24 14:49:41 -0400

    feat: button to run functions

--- src/attacher.js
+++ src/attacher.js
@@ -1,14 +1,20 @@
-js_implem = require("./js_implementation.js")
-c_implem =require("./c_implementation.js")
-document = require("../index.html")
+import * as js_implem from "./js_implementation"
+import * as c_implem from "./c_implementation"
 
-// tengo el array sacado del frontend
+// Leer array del frontend
+function parseInput(textField){
+  return textField.value.split(',').map(Number)
+}
 // arr
+
+const textField = document.getElementById('numbers')
+const buttonCalc = document.getElementById('calcButton')
+buttonCalc.addEventListener('click', () => console.log(parseInput(textField)))
 
 if (implementation == "js") {
   let result = js_implem.caller(arr);
 } else {
-  let = result = c_implem.caller(arr);
+  let result = c_implem.caller(arr);
 }
 
 // se muestra el resultado en el frontend
