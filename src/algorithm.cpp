880d0960b7b293c0d870e735c1e6df7f3bbe2547
mfocqueteau <mfocqueteau@uc.cl>
2022-04-24 17:56:38 -0400

    test 1

--- src/algorithm.cpp
+++ src/algorithm.cpp
@@ -10,13 +10,11 @@ static int pos2 = -1;
 
 // This function returns true if the array
 // can be divided into three equal sum segments
-bool equiSumUtil(int arr[], int n)
-{
+bool equiSumUtil(int arr[], int n) {
   // Prefix Sum Arrays
   int pre[n];
   int sum = 0;
-  for (int i = 0; i < n; i++)
-  {
+  for (int i = 0; i < n; i++) {
     sum += arr[i];
     pre[i] = sum;
   }
@@ -24,8 +22,7 @@ bool equiSumUtil(int arr[], int n)
   // Suffix Sum Array
   int suf[n];
   sum = 0;
-  for (int i = n - 1; i >= 0; i--)
-  {
+  for (int i = n - 1; i >= 0; i--) {
     sum += arr[i];
     suf[i] = sum;
   }
@@ -34,40 +31,28 @@ bool equiSumUtil(int arr[], int n)
   int total_sum = sum;
 
   int i = 0, j = n - 1;
-  while (i < j - 1)
-  {
-
-    if (pre[i] == total_sum / 3)
-    {
+  while (i < j - 1) {
+    if (pre[i] == total_sum / 3) {
       pos1 = i;
     }
 
-    if (suf[j] == total_sum / 3)
-    {
+    if (suf[j] == total_sum / 3) {
       pos2 = j;
     }
 
-    if (pos1 != -1 && pos2 != -1)
-    {
-
+    if (pos1 != -1 && pos2 != -1) {
       // We can also take pre[pos2 - 1] - pre[pos1] ==
       // total_sum / 3 here.
-      if (suf[pos1 + 1] - suf[pos2] == total_sum / 3)
-      {
+      if (suf[pos1 + 1] - suf[pos2] == total_sum / 3) {
         return true;
-      }
-      else
-      {
+      } else {
         return false;
       }
     }
 
-    if (pre[i] < suf[j])
-    {
+    if (pre[i] < suf[j]) {
       i++;
-    }
-    else
-    {
+    } else {
       j--;
     }
   }
@@ -75,46 +60,38 @@ bool equiSumUtil(int arr[], int n)
   return false;
 }
 
-string equiSum(int arr[], int n)
-{
+string equiSum(int arr[], int n) {
   bool ans = equiSumUtil(arr, n);
   string answer = "";
 
-  if (ans)
-  {
-    answer << "First Segment : ";
-    for (int i = 0; i <= pos1; i++)
-    {
-      answer << arr[i] << ", ";
+  if (ans) {
+    answer += "First Segment : ";
+    for (int i = 0; i <= pos1; i++) {
+      answer += arr[i] + ", ";
     }
 
-    answer << "and ";
+    answer += "and ";
 
-    answer << "Second Segment : ";
-    for (int i = pos1 + 1; i < pos2; i++)
-    {
-      answer << arr[i] << ", ";
+    answer += "Second Segment : ";
+    for (int i = pos1 + 1; i < pos2; i++) {
+      answer += arr[i] + ", ";
     }
 
-    answer << "and ";
+    answer += "and ";
 
-    answer << "Third Segment : ";
-    for (int i = pos2; i < n; i++)
-    {
-      answer << arr[i] << ", ";
+    answer += "Third Segment : ";
+    for (int i = pos2; i < n; i++) {
+      answer += arr[i] + ", ";
     }
-  }
-  else
-  {
-    answer << "Array cannot be divided into three equal sum segments";
+  } else {
+    answer = "Array cannot be divided into three equal sum segments";
   }
 
   return answer;
 }
 
 // Driver code
-string caller(int arr[])
-{
+string caller(int arr[]) {
   int n = sizeof(arr) / sizeof(arr[0]);
   return equiSum(arr, n);
 }
